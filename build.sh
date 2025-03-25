ab019d3fb5ef8f86f04a1296965d3b7d4952aa8e
zengkv <740832946@qq.com>
2025-03-25 11:51:14 +0800

    fix:完善build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 apt install libboost-all-dev
+apt install openssl
+apt install libssl-dev
 apt install libopus-dev
 apt install libogg-dev
+git submodule update --init --recursive
+mkdir build
+cd build
+cmake ..  # or cmake -DCMAKE_BUILD_TYPE=Release ..
+make
